0a0ebb057b089d784c321e24ad34f5af392beefc
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2024-07-21 21:28:27 +0530

    feat: implement registeruser and login for post and get API end points respectively

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Post, Body, Get } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { RegisterUserDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post()
+  create(@Body() registerData: RegisterUserDto) {
+    return this.authService.register(registerData);
+  }
+
+  @Get()
+  login(@Body() loginData: LoginDto) {
+    return this.authService.login(loginData);
+  }
+}
