ffd7beb151677a199c594e1c2f2a68ab0e9359e1
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2024-07-24 03:16:24 +0530

    feat: add email to the userdata to be returned whle creating the todo successfully

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -8,22 +8,34 @@ import { Prisma } from '@prisma/client';
 export class TodoService {
   constructor(private readonly databaseService: DatabaseService) {}
 
-  async create(createTodoDto: CreateTodoDto) {
+  async create(createTodoDto: CreateTodoDto, email: string) {
     try {
+      const user = await this.databaseService.user.findUnique({
+        where: { email },
+      });
+      if (!user) {
+        throw new Error('User not found');
+      }
       const data: Prisma.TodoCreateInput = {
         description: createTodoDto.description,
         task: createTodoDto.task,
         status: 'ACTIVE',
+        user: {
+          connect: { email: user.email },
+        },
       };
-      console.log(data);
       return await this.databaseService.todo.create({ data });
     } catch (err) {
       return err;
     }
   }
 
-  async findAll() {
-    return this.databaseService.todo.findMany();
+  async findAll(userEmail: string) {
+    return this.databaseService.todo.findMany({
+      where: {
+        userEmail: userEmail,
+      },
+    });
   }
 
   async findOne(id: number) {
