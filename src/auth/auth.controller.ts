025a27112f0c8fedf7413dcb0811eeb9aa07823c
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2024-07-24 04:16:00 +0530

    feat: add apitags and api operation with description and summary to login and register endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,18 +1,28 @@
-import { Controller, Post, Body, Get } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterUserDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post()
+  @Post('register')
+  @ApiOperation({
+    description: 'To register a new user with email.',
+    summary: 'Register a new User with details',
+  })
   create(@Body() registerData: RegisterUserDto) {
     return this.authService.register(registerData);
   }
 
-  @Get()
+  @Post('login')
+  @ApiOperation({
+    description: 'Login using Email',
+    summary: 'Endpoint to Login with User Email and Password',
+  })
   login(@Body() loginData: LoginDto) {
     return this.authService.login(loginData);
   }
