c96130eeecedd1b5614893d1092ddbbe09d65912
harmeetsingh11 <itsharmeetsingh1@gmail.com>
2024-07-24 04:22:52 +0530

    feat: add ApiBearerAuth, ApiTags, ApiOperation with description and summary at each todo api endpoint

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -13,36 +13,63 @@ import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
 import { JwtAuthGuard } from 'src/auth/auth.guard';
 import { UserEmail } from 'src/common/decorator/user-email.decorator';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Todo')
 @Controller('todo')
 export class TodoController {
   constructor(private readonly todoService: TodoService) {}
 
+  @ApiBearerAuth()
   @UseGuards(JwtAuthGuard)
+  @ApiOperation({
+    description: 'To create a new todo/task wrt user email',
+    summary: 'Create a new Todo/Task',
+  })
   @Post()
   create(@Body() createTodoDto: CreateTodoDto, @UserEmail() userEmail: string) {
     return this.todoService.create(createTodoDto, userEmail);
   }
 
+  @ApiBearerAuth()
   @UseGuards(JwtAuthGuard)
+  @ApiOperation({
+    description: "To get all the user's todos/tasks",
+    summary: "To get all the user's todos/tasks",
+  })
   @Get()
   findAll(@UserEmail() userEmail: string) {
     return this.todoService.findAll(userEmail);
   }
 
+  @ApiBearerAuth()
   @UseGuards(JwtAuthGuard)
+  @ApiOperation({
+    description: 'To get a specific user todo/task',
+    summary: 'To get a specific user todo/task',
+  })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.todoService.findOne(+id);
   }
 
+  @ApiBearerAuth()
   @UseGuards(JwtAuthGuard)
+  @ApiOperation({
+    description: 'To update a specific user todo/task',
+    summary: 'To update a specific user todo/task',
+  })
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateTodoDto: UpdateTodoDto) {
     return this.todoService.update(+id, updateTodoDto);
   }
 
+  @ApiBearerAuth()
   @UseGuards(JwtAuthGuard)
+  @ApiOperation({
+    description: 'To delete a specific user todo/task',
+    summary: 'To delete a specific user todo/task',
+  })
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.todoService.remove(+id);
